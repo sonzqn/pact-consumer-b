478436c7ffebae63aab41fcbe5f408258d15aa4c
sonzqn <sonzqn@gmail.com>
2022-04-11 20:54:26 +0700

    update

--- tests/test_user_consumer.py
+++ tests/test_user_consumer.py
@@ -12,11 +12,19 @@ from src.consumer import UserConsumer
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+import socket
+from contextlib import closing
+
+def find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
 
 # Define where to run the mock server, for the consumer to connect to. These
 # are the defaults so may be omitted
 PACT_MOCK_HOST = "localhost"
-PACT_MOCK_PORT = 1234
+PACT_MOCK_PORT = find_free_port()
 
 # Where to output the JSON Pact files created by any tests
 PACT_DIR = os.path.dirname(os.path.realpath(__file__)) + "/pacts"
